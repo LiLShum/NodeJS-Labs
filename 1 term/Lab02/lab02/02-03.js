5026ac68ec025634446d64ec97c544d6fc407e1d
Nikita <dimbildon2281337@gmail.com>
2024-09-10 12:45:03 +0300

    Labs

---
+++ 1 term/Lab02/lab02/02-03.js
@@ -0,0 +1,12 @@
+let http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === "/api/name" && req.method === 'GET') {
+        res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8;"});
+        res.end("Фамилия Имя Отчество");
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html; charset=utf-8;"});
+        res.end("<h2>Not found</h2>");
+    }
+}).listen(5000, () => console.log("Server is running at http://localhost:5000"));
+
